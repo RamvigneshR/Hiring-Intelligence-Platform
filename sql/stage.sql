addbf3c48d2a1a8b606bc4a5a4dea00ab9e13ecc
RamvigneshR <93775774+RamvigneshR@users.noreply.github.com>
2026-02-21 10:42:23 +0530

    Stage_jobs python script and sql query

--- sql/stage.sql
+++ sql/stage.sql
@@ -11,6 +11,7 @@ create table if not exists stage.stage_jobs(
     country text,
     is_remote boolean,
     posted_time date not null,
+    posted_date_key integer not null,
     job_description text ,
     experience_level text,
     source_url text,
@@ -18,5 +19,32 @@ create table if not exists stage.stage_jobs(
     run_id text,
     dss_update_time timestamp default now()
 );
-
 select * from stage.stage_jobs;
+create index if not exists ids_stage_jobs on stage.stage_jobs(job_id,company_name,posted_date_key);
+create table if not exists stage.stage_fact_jobs(
+    
+    job_id text not null,
+    run_id text not null,
+    company_key integer,
+    company_name text,
+    location_key integer,
+    location text,
+    job_title_key integer,
+    job_title text,
+    posted_date_key integer,
+    
+    is_remote boolean,
+    experience_level text,
+    source_url text,
+    run_date date,
+    
+    is_valid boolean default true,
+    validation_error text,
+    dss_load_time timestamp default current_timestamp
+);
+
+create index if not exists idx_stage_fact_jobs on stage.stage_fact_jobs(job_id,run_id);
+
+select * from stage.stage_fact_jobs
+order by dss_load_time desc;
+
